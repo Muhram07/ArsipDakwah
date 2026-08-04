46c8ef52003fbe1ce5a57ab6c321ad6e89090cac
Muhram07 <126127050+Muhram07@users.noreply.github.com>
2026-08-04 22:30:33 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,39 @@
-hereconsole.log("Arsip Dakwah siap.");
+async function loadPosters() {
+
+  const container = document.getElementById("post-list");
+
+  try {
+
+    const res = await fetch("data/posters.json");
+
+    const posters = await res.json();
+
+    container.innerHTML = "";
+
+    posters.forEach(item => {
+
+      container.innerHTML += `
+      <div class="poster">
+
+        <img src="${item.image}" alt="${item.title}">
+
+        <h3>${item.title}</h3>
+
+        <p>${item.caption}</p>
+
+        <small>${item.category}</small>
+
+      </div>
+      `;
+
+    });
+
+  } catch (e) {
+
+    container.innerHTML = "Gagal memuat data.";
+
+  }
+
+}
+
+loadPosters();
