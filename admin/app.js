a71a8c2bbf82662f7e9d4c7a573b73cdc5661d0a
Muhram07 <126127050+Muhram07@users.noreply.github.com>
2026-08-05 15:01:11 +0700

    Create app.js

---
+++ admin/app.js
@@ -0,0 +1,11 @@
+/* ===========================
+   ADMIN PANEL
+=========================== */
+
+console.log("Admin Panel Aktif");
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    console.log("Dashboard Siap");
+
+});
