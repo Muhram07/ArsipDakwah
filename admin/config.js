badf469f2f96cee4a4e40ad1f914c1c97c3befb1
Muhram07 <126127050+Muhram07@users.noreply.github.com>
2026-08-05 15:07:36 +0700

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,16 +1,9 @@
-here/* ===========================
+/* ===========================
    KONFIGURASI ADMIN
 =========================== */
 
 const ADMIN_CONFIG = {
-
-    // Ganti PIN sesuai keinginan
     PIN: "123456",
-
-    // Nama Admin
     NAME: "Muhram",
-
-    // Versi Panel
     VERSION: "1.0"
-
 };
